df8ff8cae8be2ba0c118939ecb5881d199910693
1707rosa <160504503+1707rosa@users.noreply.github.com>
2024-10-22 22:57:23 -0700

    Creation

---
+++ source/AutoInsure/AutoInsure.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AutoInsure.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
